bc121c91118f4d10d5accfab31cb11db2cfc6c45
cibermarcoa <marcoantonio.dzcl@gmail.com>
2024-12-21 14:19:57 +0100

    Clases hechas

---
+++ BurgerSelfOrderKiosk/src/BurgerSelfOrderKiosk.java
@@ -0,0 +1,23 @@
+
+import manager.KioskManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author cibermarcoa
+ */
+public class BurgerSelfOrderKiosk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        KioskManager kioskManager = new KioskManager();
+        kioskManager.start();
+    }
+    
+}
---
+++ BurgerSelfOrderKiosk/src/products/MenuCardSection.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package products;
+
+/**
+ *
+ * @author nemo
+ */
+public class MenuCardSection {
+    /*
+    -sectionName: String
+-imageFileName: String
+-productList: List<IndividualProduct>
++getProduct(int): InidivudualProduct
++getSectionName()
++getNumberOfProducts(): int
++getImageFileNmae(): String
++MenuCardSection(name: Str, imageFileName: Str, prods: List<IndividualProduct>
+        */
+}
