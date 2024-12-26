76ac9bb503dfcf2d7328bfc05723bad160252523
hugomn4 <hugo.mn2004@gmail.com>
2024-12-26 17:40:40 +0100

    ya se puede usar el boton menu solo que hay que retocar

--- BurgerSelfOrderKiosk/src/sreens/MenuScreen.java
+++ BurgerSelfOrderKiosk/src/sreens/MenuScreen.java
@@ -12,71 +12,41 @@ import products.MenuCard;
  *
  * @author nemo
  */
-/*public class MenuScreen implements KioskScreen {
-
-    private int burgerSelected = -1;  
-    private int drinkSelected = -1;    
-    private int sideSelected = -1;    
+public class MenuScreen implements KioskScreen {
 
     @Override
     public KioskScreen show(Context c) {
         SimpleKiosk k = c.getKiosk();
-        k.clearScreen();
         char res = '\0';
+        
+        // Mostrar opciones del menú principal
+        k.clearScreen();
+        k.setTitle("Menú Principal");
+        k.setDescription("Elige una categoría:");
 
-        while (burgerSelected == -1 || drinkSelected == -1 || sideSelected == -1) {
-            k.clearScreen();
-            k.setTitle("Selecciona un producto para tu menú");
+        // Mostrar opciones
+        k.setOption(1, "Hamburguesas");
+        k.setOption(2, "Bebidas");
+        k.setOption(3, "Complementos");
+        k.setOption(4, "Ver Pedido");
+        k.setOption(5, "Salir");
 
-            if (burgerSelected == -1) {
-                k.setOption(0, "Selecciona una hamburguesa");
-            } else {
-                k.setOption(0, "Hamburguesa seleccionada");
-            }
+        // Esperar la selección del usuario
+        res = k.waitEvent(60);
 
-            if (drinkSelected == -1) {
-                k.setOption(1, "Selecciona una bebida");
-            } else {
-                k.setOption(1, "Bebida seleccionada");
-            }
-
-            if (sideSelected == -1) {
-                k.setOption(2, "Selecciona un complemento");
-            } else {
-                k.setOption(2, "Complemento seleccionado");
-            }
-
-            res = k.waitEvent(60);
-
-            if (res == 'A' && burgerSelected == -1) {
-                burgerSelected = selectProduct(c, 0);  
-            } else if (res == 'B' && drinkSelected == -1) {
-                drinkSelected = selectProduct(c, 1);  
-            } else if (res == 'C' && sideSelected == -1) {
-                sideSelected = selectProduct(c, 2);  
-            }
+        // Manejar la selección
+        if (res == 'A') { // Hamburguesas
+            return new SectionScreen();
+        } else if (res == 'B') { // Bebidas
+            return new SectionScreen();
+        } else if (res == 'C') { // Complementos
+            return new SectionScreen();
+        } else if (res == 'D') { // Ver Pedido
+            return new OrderScreen();
+        } else if (res == 'E') { // Salir
+            return new WellcomeScreen(); 
         }
 
-        addMenuToOrder(c);
-
-        return new OrderScreen();  
+        return null;
     }
-
-    private int selectProduct(Context c, int section) {
-        ProductScreen productScreen = new ProductScreen(section);
-        productScreen.show(c);  
-        return section;  
-    }
-
-    private void addMenuToOrder(Context c) {
-        int burgerSelected = 0; 
-        int drinkSelected = 1;  
-        int sideSelected = 2;   
-        
-        MenuCard menuCard = MenuCard.loadFromDisk();  
-
-        c.getOrder().addMenu(burgerSelected, drinkSelected, sideSelected, menuCard);
-    }
-
 }
-*/
