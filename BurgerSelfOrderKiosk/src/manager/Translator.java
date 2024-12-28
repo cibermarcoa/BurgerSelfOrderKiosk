f413c65a556ee8d9a11c51349cb56177de1ef05b
hugomn4 <hugo.mn2004@gmail.com>
2024-12-28 12:06:26 +0100

    translator y translator manager

--- BurgerSelfOrderKiosk/src/manager/Translator.java
+++ BurgerSelfOrderKiosk/src/manager/Translator.java
@@ -4,10 +4,41 @@
  */
 package manager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author cibermarcoa
  */
 public class Translator {
-    
+
+    private Map<String, String> palabras = new HashMap<>();
+
+    public Translator(String filename) {
+        try {
+            Scanner scannerFile = new Scanner(new File(filename));
+            while (scannerFile.hasNext()) {
+                String line = scannerFile.nextLine();
+                String[] textTranslation = line.split(":");
+                if (textTranslation.length == 2) {
+                    palabras.put(textTranslation[0], textTranslation[1]);
+                }
+            }
+            scannerFile.close(); // Asegúrate de cerrar el Scanner
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se encuentra el fichero de idiomas: " + filename);
+        }
+    }
+
+    public String translate(String cad) {
+        if (this.palabras.containsKey(cad)) {
+            return palabras.get(cad);
+        } else {
+            return cad;
+        }
+    }
 }
--- BurgerSelfOrderKiosk/src/manager/TranslatorManager.java
+++ BurgerSelfOrderKiosk/src/manager/TranslatorManager.java
@@ -4,10 +4,46 @@
  */
 package manager;
 
-/**
- *
- * @author cibermarcoa
- */
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TranslatorManager {
-    
+    private Translator currentDic = null;
+    private Map<String, Translator> dictionaries = new HashMap<>();
+
+    public TranslatorManager() {
+        String rutaDirectorioDic = "DICCIONARIOS";
+        File directorio = new File(rutaDirectorioDic);
+        if (directorio.exists() && directorio.isDirectory()) {
+            File[] ficheros = directorio.listFiles();
+            for (File fich : ficheros) {
+                if (fich.getName().contains(".txt")) {
+                    Translator nuevoTraductor = new Translator(fich.getPath());
+                    String nombreDelDic = fich.getName().substring(0, fich.getName().length() - 4);
+                    dictionaries.put(nombreDelDic, nuevoTraductor);
+                    System.out.println(fich.getName());
+                }
+            }
+        }
+    }
+
+    public void setCurrentIdiom(String idiom) {
+        this.currentDic = this.dictionaries.get(idiom);
+    }
+
+    public String translate(String cad) {
+        if (this.currentDic != null) {
+            return currentDic.translate(cad);
+        }
+        return cad;
+    }
+
+    public List<String> getIdioms() {
+        List<String> idioms = new ArrayList<>();
+        idioms.addAll(dictionaries.keySet());
+        return idioms;
+    }
 }
